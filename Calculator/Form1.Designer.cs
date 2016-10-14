ef2aacc9e510ac5164bf7ba4748fb0342a239ae2
Adam Gothmann <adamgothmann@gmail.com>
2016-10-14 09:50:52 -0500

    initial commit

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OneButton = new System.Windows.Forms.Button();
+            this.TwoButton = new System.Windows.Forms.Button();
+            this.ThreeButton = new System.Windows.Forms.Button();
+            this.FourButton = new System.Windows.Forms.Button();
+            this.FiveButton = new System.Windows.Forms.Button();
+            this.SixButton = new System.Windows.Forms.Button();
+            this.SevenButton = new System.Windows.Forms.Button();
+            this.EightButton = new System.Windows.Forms.Button();
+            this.NineButton = new System.Windows.Forms.Button();
+            this.ZeroButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OneButton
+            // 
+            this.OneButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OneButton.Location = new System.Drawing.Point(12, 210);
+            this.OneButton.Name = "OneButton";
+            this.OneButton.Size = new System.Drawing.Size(35, 50);
+            this.OneButton.TabIndex = 0;
+            this.OneButton.Text = "1";
+            this.OneButton.UseVisualStyleBackColor = true;
+            // 
+            // TwoButton
+            // 
+            this.TwoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TwoButton.Location = new System.Drawing.Point(62, 210);
+            this.TwoButton.Name = "TwoButton";
+            this.TwoButton.Size = new System.Drawing.Size(35, 50);
+            this.TwoButton.TabIndex = 1;
+            this.TwoButton.Text = "2";
+            this.TwoButton.UseVisualStyleBackColor = true;
+            // 
+            // ThreeButton
+            // 
+            this.ThreeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ThreeButton.Location = new System.Drawing.Point(114, 210);
+            this.ThreeButton.Name = "ThreeButton";
+            this.ThreeButton.Size = new System.Drawing.Size(35, 50);
+            this.ThreeButton.TabIndex = 2;
+            this.ThreeButton.Text = "3";
+            this.ThreeButton.UseVisualStyleBackColor = true;
+            // 
+            // FourButton
+            // 
+            this.FourButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FourButton.Location = new System.Drawing.Point(12, 154);
+            this.FourButton.Name = "FourButton";
+            this.FourButton.Size = new System.Drawing.Size(35, 50);
+            this.FourButton.TabIndex = 3;
+            this.FourButton.Text = "4";
+            this.FourButton.UseVisualStyleBackColor = true;
+            // 
+            // FiveButton
+            // 
+            this.FiveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FiveButton.Location = new System.Drawing.Point(62, 154);
+            this.FiveButton.Name = "FiveButton";
+            this.FiveButton.Size = new System.Drawing.Size(35, 50);
+            this.FiveButton.TabIndex = 4;
+            this.FiveButton.Text = "5";
+            this.FiveButton.UseVisualStyleBackColor = true;
+            // 
+            // SixButton
+            // 
+            this.SixButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SixButton.Location = new System.Drawing.Point(114, 154);
+            this.SixButton.Name = "SixButton";
+            this.SixButton.Size = new System.Drawing.Size(35, 50);
+            this.SixButton.TabIndex = 5;
+            this.SixButton.Text = "6";
+            this.SixButton.UseVisualStyleBackColor = true;
+            // 
+            // SevenButton
+            // 
+            this.SevenButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SevenButton.Location = new System.Drawing.Point(12, 98);
+            this.SevenButton.Name = "SevenButton";
+            this.SevenButton.Size = new System.Drawing.Size(35, 50);
+            this.SevenButton.TabIndex = 6;
+            this.SevenButton.Text = "7";
+            this.SevenButton.UseVisualStyleBackColor = true;
+            // 
+            // EightButton
+            // 
+            this.EightButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EightButton.Location = new System.Drawing.Point(62, 98);
+            this.EightButton.Name = "EightButton";
+            this.EightButton.Size = new System.Drawing.Size(35, 50);
+            this.EightButton.TabIndex = 7;
+            this.EightButton.Text = "8";
+            this.EightButton.UseVisualStyleBackColor = true;
+            // 
+            // NineButton
+            // 
+            this.NineButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NineButton.Location = new System.Drawing.Point(114, 98);
+            this.NineButton.Name = "NineButton";
+            this.NineButton.Size = new System.Drawing.Size(35, 50);
+            this.NineButton.TabIndex = 8;
+            this.NineButton.Text = "9";
+            this.NineButton.UseVisualStyleBackColor = true;
+            // 
+            // ZeroButton
+            // 
+            this.ZeroButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZeroButton.Location = new System.Drawing.Point(12, 265);
+            this.ZeroButton.Name = "ZeroButton";
+            this.ZeroButton.Size = new System.Drawing.Size(140, 50);
+            this.ZeroButton.TabIndex = 9;
+            this.ZeroButton.Text = "0";
+            this.ZeroButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 17);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 75);
+            this.textBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 327);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ZeroButton);
+            this.Controls.Add(this.NineButton);
+            this.Controls.Add(this.EightButton);
+            this.Controls.Add(this.SevenButton);
+            this.Controls.Add(this.SixButton);
+            this.Controls.Add(this.FiveButton);
+            this.Controls.Add(this.FourButton);
+            this.Controls.Add(this.ThreeButton);
+            this.Controls.Add(this.TwoButton);
+            this.Controls.Add(this.OneButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OneButton;
+        private System.Windows.Forms.Button TwoButton;
+        private System.Windows.Forms.Button ThreeButton;
+        private System.Windows.Forms.Button FourButton;
+        private System.Windows.Forms.Button FiveButton;
+        private System.Windows.Forms.Button SixButton;
+        private System.Windows.Forms.Button SevenButton;
+        private System.Windows.Forms.Button EightButton;
+        private System.Windows.Forms.Button NineButton;
+        private System.Windows.Forms.Button ZeroButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
